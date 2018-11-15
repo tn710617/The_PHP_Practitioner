85188ca2c9a55005dfa639c221f4d54cafd2f503
Ray <tn710617@gmail.com>
2018-11-15 12:09:54 +0800

    Make a prototype of PDO

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'functions.php';
+
+require_once 'index.view.php';
+
+try {
+    $pdo = new PDO('mysql:host=127.0.0.1;dbname=The_PHP_Practitioner', 'root', '');
+} catch (PDOException $e)
+{
+    die('could not connect');
+}
