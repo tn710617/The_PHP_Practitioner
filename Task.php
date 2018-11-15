763629958c67be2227ea60d5d086a009bfe3a2d7
Ray <tn710617@gmail.com>
2018-11-15 14:20:15 +0800

    Make pdo connection a function

--- Task.php
+++ Task.php
@@ -6,4 +6,9 @@ class Task {
     public $description;
     public $completed;
 
+    public function foobar ()
+    {
+        return 'foobar';
+    }
+
 }
--- index.php
+++ index.php
@@ -3,13 +3,7 @@ require_once 'functions.php';
 require_once 'Task.php';
 
 
-try {
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=The_PHP_Practitioner', 'root', '');
-} catch (PDOException $e)
-{
-    die($e->getMessage());
-}
-
+$pdo = connectToDb();
 $statement = $pdo->prepare('select * from mytodo');
 $statement->execute();
 
