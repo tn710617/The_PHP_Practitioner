a050bb8bbf6bae75ff9f4606bf8dea208a43df11
Ray <tn710617@gmail.com>
2018-11-16 16:43:01 +0800

    Add ATTRMODE into config file to show error when table is not found

--- config.php
+++ config.php
@@ -5,6 +5,6 @@ return [
         'username' => 'root',
         'password' => '',
         'connection' => 'mysql:host=127.0.0.1',
-        'options' => []
+        'options' => [PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING]
     ]
 ];
