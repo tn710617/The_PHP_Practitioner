557840060acf5cbf704f9da36b393b1e6a7abd67
Ray <tn710617@gmail.com>
2018-11-19 11:28:47 +0800

    Complete array_filter example

---
+++ Filtering_arrays/ararys.php
@@ -0,0 +1,25 @@
+<?php
+class Post
+{
+    public $title;
+    public $published;
+
+    public function __construct($title, $published)
+    {
+        $this->title = $title;
+        $this->published = $published;
+    }
+}
+
+$posts = [
+    new Post('My first post', true),
+    new Post('My second post', true),
+    new Post('My third post', true),
+    new Post('My fourth post', false)
+];
+
+$unpublished= array_filter($posts, function($post) {
+    return ! $post->published;
+});
+
+var_dump($unpublished);
